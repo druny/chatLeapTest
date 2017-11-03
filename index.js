3d1fe5a1068d7ac43ca82a61e35b1187f28ab37c
Andrew <druny195@rambler.ru>
2017-11-03 15:27:44 +0200

    Add start app endpoint

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 require('./service/mongo');
+require('./service/queue/process');
 
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
+const kue = require('kue');
+const shedule = require('node-schedule');
+
+const Job = require('./model/Job');
+const createJobs = require('./service/queue/create');
+
+shedule.scheduleJob('0 * * * *', () => (
+  Job.find({})
+    .populate('users')
+    .then(createJobs)
+    .catch(console.log)
+));
+
+kue.app.listen(3000);
+
+cluster.on('death', worker => {
+  console.log('worker pid:' + worker.pid + ' died!'.bold.red);
+});
